ea09d914389267ec91c7b7d6d47c53e13f507d1a
saranshane <saranshane01@gmail.com>
2024-04-30 13:46:54 +0530

    commit from laptop

--- lib/Homescreen/Inbox_Body.dart
+++ lib/Homescreen/Inbox_Body.dart
@@ -1,10 +1,144 @@
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:login/models/chart.dart';
+// import 'package:login/Message_Screen/MessageScreen.dart';
+// import 'package:login/constants.dart';
+
+// class Body extends StatefulWidget {
+//   @override
+//   _BodyState createState() => _BodyState();
+// }
+
+// class _BodyState extends State<Body> {
+//   List<Conversation> conversations = []; // List to store fetched chat data
+//   bool isLoading = false; // Flag to track loading state
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchChats(); // Fetch chat data when the widget initializes
+//   }
+
+//   Future<void> fetchChats() async {
+//     setState(() {
+//       isLoading = true; // Set loading flag to true
+//     });
+
+//     try {
+//       final response = await http.get(
+//           Uri.parse('http://localhost:3000/api/conversations/?userId=660be75b9940f152d687a95b')); // Replace 'YOUR_BACKEND_API_URL' with your actual backend API URL
+
+//       if (response.statusCode == 200) {
+//         final Map<String, dynamic> responseData = json.decode(response.body); // Parse JSON response
+//         final List<dynamic> conversationList = responseData['conversations'];
+//         setState(() {
+//           conversations = conversationList.map((data) => Conversation.fromJson(data)).toList(); // Convert JSON data to List<Conversation>
+//           isLoading = false; // Set loading flag to false
+//         });
+//       } else {
+//         // Handle error
+//         print('Failed to load chat data');
+//         setState(() {
+//           isLoading = false; // Set loading flag to false in case of error
+//         });
+//       }
+//     } catch (e) {
+//       // Handle network or parsing errors
+//       print('Error fetching chat data: $e');
+//       setState(() {
+//         isLoading = false; // Set loading flag to false in case of error
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Expanded(
+//           child: isLoading
+//               ? Center(child: CircularProgressIndicator()) // Show loading indicator while fetching data
+//               : ListView.builder(
+//                   itemCount: conversations.length,
+//                   itemBuilder: (context, index) => ChatCard(
+//                     conversation: conversations[index],
+//                     press: () =>
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => MessagesScreen(),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+// class ChatCard extends StatelessWidget {
+//   final Conversation conversation;
+//   final VoidCallback press;
+
+//   const ChatCard({
+//     Key? key,
+//     required this.conversation,
+//     required this.press,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: press,
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
+//         child: Row(
+//           children: [
+//             CircleAvatar(
+//               radius: 24,
+//               // backgroundImage: AssetImage(conversation.recipients[0].image), // Assuming the first recipient's image is used
+//             ),
+//             Expanded(
+//               child: Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       conversation.recipients[1].name, // Assuming the first recipient's name is used
+//                       style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+//                     ),
+//                     SizedBox(height: 8),
+//                     Opacity(
+//                       opacity: 0.64,
+//                       child: Text(
+//                         conversation.text, // Assuming the conversation text is used
+//                         maxLines: 1,
+//                         overflow: TextOverflow.ellipsis,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             Opacity(
+//               opacity:0.64,
+//               child: Text(conversation.updatedAt.toString()), // Assuming updatedAt is used
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:login/models/chart.dart'; 
+import 'package:login/models/chart.dart';
 import 'package:login/Message_Screen/MessageScreen.dart';
 import 'package:login/constants.dart';
-
 
 class Body extends StatefulWidget {
   @override
@@ -27,14 +161,17 @@ class _BodyState extends State<Body> {
     });
 
     try {
-      final response = await http.get(
-          Uri.parse('http://localhost:3000/api/conversations/?userId=660be75b9940f152d687a95b')); // Replace 'YOUR_BACKEND_API_URL' with your actual backend API URL
+      final response = await http.get(Uri.parse(
+          'http://localhost:3000/api/conversations/?userId=660be75b9940f152d687a95b')); // Replace 'YOUR_BACKEND_API_URL' with your actual backend API URL
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> responseData = json.decode(response.body); // Parse JSON response
+        final Map<String, dynamic> responseData =
+            json.decode(response.body); // Parse JSON response
         final List<dynamic> conversationList = responseData['conversations'];
         setState(() {
-          conversations = conversationList.map((data) => Conversation.fromJson(data)).toList(); // Convert JSON data to List<Conversation>
+          conversations = conversationList
+              .map((data) => Conversation.fromJson(data))
+              .toList(); // Convert JSON data to List<Conversation>
           isLoading = false; // Set loading flag to false
         });
       } else {
@@ -59,13 +196,14 @@ class _BodyState extends State<Body> {
       children: [
         Expanded(
           child: isLoading
-              ? Center(child: CircularProgressIndicator()) // Show loading indicator while fetching data
+              ? Center(
+                  child:
+                      CircularProgressIndicator()) // Show loading indicator while fetching data
               : ListView.builder(
                   itemCount: conversations.length,
                   itemBuilder: (context, index) => ChatCard(
                     conversation: conversations[index],
-                    press: () => 
-                    Navigator.push(
+                    press: () => Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => MessagesScreen(),
@@ -78,6 +216,7 @@ class _BodyState extends State<Body> {
     );
   }
 }
+
 class ChatCard extends StatelessWidget {
   final Conversation conversation;
   final VoidCallback press;
@@ -93,7 +232,8 @@ class ChatCard extends StatelessWidget {
     return InkWell(
       onTap: press,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
+        padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
         child: Row(
           children: [
             CircleAvatar(
@@ -102,19 +242,23 @@ class ChatCard extends StatelessWidget {
             ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      conversation.recipients[1].name, // Assuming the first recipient's name is used
-                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                      conversation.recipients[1]
+                          .name, // Assuming the first recipient's name is used
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                     ),
                     SizedBox(height: 8),
                     Opacity(
                       opacity: 0.64,
                       child: Text(
-                        conversation.text, // Assuming the conversation text is used
+                        conversation
+                            .text, // Assuming the conversation text is used
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -124,8 +268,9 @@ class ChatCard extends StatelessWidget {
               ),
             ),
             Opacity(
-              opacity:0.64,
-              child: Text(conversation.updatedAt.toString()), // Assuming updatedAt is used
+              opacity: 0.64,
+              child: Text(conversation.updatedAt
+                  .toString()), // Assuming updatedAt is used
             ),
           ],
         ),
