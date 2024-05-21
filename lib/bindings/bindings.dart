ae94cdb8bbd06db880c300347371f9538b84e588
saranshane <saranshane01@gmail.com>
2024-05-21 08:41:37 +0530

    bindings

--- lib/bindings/bindings.dart
+++ lib/bindings/bindings.dart
@@ -10,11 +10,16 @@ import 'package:login/settings/settings_screen.dart';
 
 final List<GetPage> appPages = [
   GetPage(name: '/EntryScreen', page: () => EntryScreen()), // Entry Screen
-  GetPage(name: '/onboarding', page: () => OnboardingScreen()), // Onboarding Screen
+  GetPage(
+      name: '/onboarding', page: () => OnboardingScreen()), // Onboarding Screen
   GetPage(name: '/login', page: () => LoginScreen()), // Login Screen
   GetPage(name: '/otp', page: () => OtpScreen()), // OTP Screen
-  GetPage(name: '/Registration', page: () => RegistrationScreen()), // Registration Screen
+  GetPage(
+      name: '/Registration',
+      page: () => RegistrationScreen()), // Registration Screen
   GetPage(name: '/HomeScreen', page: () => HomeScreen()), // Home Screen
-  GetPage(name: '/ProfileScreen', page: () => ProfileScreen()) // Profile Screen
-  GetPage(name: '/settingsscreen', page: () => SettingsScreen())//Settings Screen
+  GetPage(
+      name: '/ProfileScreen', page: () => ProfileScreen()), // Profile Screen
+  GetPage(
+      name: '/settingsscreen', page: () => SettingsScreen()) //Settings Screen
 ];
