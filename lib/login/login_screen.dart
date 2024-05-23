69eeefcc7371ffa5b796be711f27d88c31f4fd51
saranshane <saranshane01@gmail.com>
2024-05-23 13:54:27 +0530

    Update login

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:login/login/otp_screen.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:login/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
@@ -21,7 +22,8 @@ class LoginScreen extends StatelessWidget {
             AppBarContent(), // Added the AppBarContent widget here
             Expanded(
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                padding: EdgeInsets.symmetric(
+                    horizontal: kDefaultPadding, vertical: kDefaultPadding + 5),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -43,16 +45,16 @@ class LoginScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Welcome Back',
+                        AppStrings.welcomeBack,
                         textAlign: TextAlign.start,
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                          color: AppColors.textColor,
                         ),
                       ),
                       Text(
-                        'Login to continue',
+                        AppStrings.loginToContinue,
                         textAlign: TextAlign.start,
                         style: TextStyle(
                           fontSize: 18,
@@ -63,7 +65,7 @@ class LoginScreen extends StatelessWidget {
                       TextFormField(
                         controller: emailController,
                         decoration: InputDecoration(
-                          labelText: 'Email',
+                          labelText: AppStrings.email,
                           prefixIcon: Icon(Icons.email),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10),
@@ -75,7 +77,7 @@ class LoginScreen extends StatelessWidget {
                         height: 20,
                         child: Center(
                           child: Text(
-                            'OR',
+                            AppStrings.or,
                             style: TextStyle(fontSize: 18),
                           ),
                         ),
@@ -84,7 +86,7 @@ class LoginScreen extends StatelessWidget {
                       IntlPhoneField(
                         controller: phoneNumberController,
                         decoration: InputDecoration(
-                          labelText: 'Phone Number',
+                          labelText: AppStrings.mobileNumber,
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
@@ -111,8 +113,8 @@ class LoginScreen extends StatelessWidget {
                           } else {
                             // Show snackbar if neither email nor phone number is provided
                             Get.snackbar(
-                              'Warning',
-                              'Please provide either email or phone number',
+                              AppStrings.warning,
+                              AppStrings.provideContact,
                               backgroundColor: Colors.red,
                               colorText: Colors.white,
                             );
@@ -135,8 +137,8 @@ class LoginScreen extends StatelessWidget {
                           decoration: BoxDecoration(
                             gradient: LinearGradient(
                               colors: [
-                                Color(0xFF9431A5),
-                                Color(0xFFAC303B),
+                                AppColors.gradientStart,
+                                AppColors.gradientMiddle,
                               ],
                               begin: Alignment.topLeft,
                               end: Alignment.topRight,
@@ -148,7 +150,7 @@ class LoginScreen extends StatelessWidget {
                                 BoxConstraints(minWidth: 88.0, minHeight: 36.0),
                             alignment: Alignment.center,
                             child: Text(
-                              'Login',
+                              AppStrings.login,
                               style:
                                   TextStyle(fontSize: 18, color: Colors.white),
                             ),
@@ -167,8 +169,8 @@ class LoginScreen extends StatelessWidget {
   }
 
   Future<void> sendOTP(String type, String contact) async {
-    // Define your backend URL
-    String url = 'http://172.20.10.5:3000/auth/sendotp';
+    // Use the URL from constants
+    String url = AppStrings.sendOtpUrl;
 
     // Prepare the request body as a Map
     Map<String, String> body = {"type": type, "contact": contact};
@@ -211,236 +213,13 @@ class AppBarContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 320,
+      height: AppDimensions.bannerHeight,
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/Banner2.png'),
+          image: AssetImage(AppStrings.appBarLoginImage),
           fit: BoxFit.fill,
         ),
       ),
     );
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:login/login/otp_screen.dart';
-// import 'package:login/constants.dart';
-
-// class LoginScreen extends StatelessWidget {
-//   final TextEditingController emailController = TextEditingController();
-//   final TextEditingController phoneNumberController = TextEditingController();
-//   final storedText = ''.obs;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         body: Column(
-//           children: [
-//             AppBarContent(),
-//             Expanded(
-//               child: Container(
-//                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-//                 decoration: BoxDecoration(
-//                   color: Colors.white,
-//                   borderRadius: BorderRadius.only(
-//                     topLeft: Radius.circular(0),
-//                     topRight: Radius.circular(0),
-//                   ),
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.grey.withOpacity(0.5),
-//                       blurRadius: 10,
-//                       spreadRadius: 2,
-//                       offset: Offset(0, 2),
-//                     ),
-//                   ],
-//                 ),
-//                 child: SingleChildScrollView(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text(
-//                         AppStrings.welcomeBack,
-//                         style: TextStyle(
-//                           fontSize: 24,
-//                           fontWeight: FontWeight.bold,
-//                           color: Colors.black,
-//                         ),
-//                       ),
-//                       Text(
-//                         AppStrings.loginToContinue,
-//                         style: TextStyle(
-//                           fontSize: 18,
-//                           color: Colors.grey[600],
-//                         ),
-//                       ),
-//                       SizedBox(height: 20),
-//                       TextFormField(
-//                         controller: emailController,
-//                         decoration: InputDecoration(
-//                           labelText: AppStrings.email,
-//                           prefixIcon: Icon(Icons.email),
-//                           border: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                           ),
-//                         ),
-//                         keyboardType: TextInputType.emailAddress,
-//                       ),
-//                       SizedBox(height: 20),
-//                       Center(
-//                         child: Text(
-//                           AppStrings.or,
-//                           style: TextStyle(fontSize: 18),
-//                         ),
-//                       ),
-//                       SizedBox(height: 20),
-//                       TextFormField(
-//                         controller: phoneNumberController,
-//                         maxLength: 10,
-//                         decoration: InputDecoration(
-//                           labelText: AppStrings.mobileNumber,
-//                           prefixIcon: Icon(Icons.phone),
-//                           border: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                           ),
-//                         ),
-//                         keyboardType: TextInputType.phone,
-//                       ),
-//                       SizedBox(height: 40),
-//                       Center(
-//                         child: ElevatedButton(
-//                           onPressed: () {
-//                             String type;
-//                             String contact;
-
-//                             if (emailController.text.isNotEmpty) {
-//                               type = 'email';
-//                               contact = emailController.text;
-//                             } else if (phoneNumberController.text.isNotEmpty) {
-//                               type = 'phoneNumber';
-//                               contact = phoneNumberController.text;
-//                             } else {
-//                               Get.snackbar(
-//                                 AppStrings.warning,
-//                                 AppStrings.provideContact,
-//                                 backgroundColor: Colors.red,
-//                                 colorText: Colors.white,
-//                               );
-//                               return;
-//                             }
-
-//                             _saveText(contact);
-//                             sendOTP(type, contact);
-//                           },
-//                           style: ElevatedButton.styleFrom(
-//                             elevation: 0,
-//                             padding: EdgeInsets.zero,
-//                             shape: RoundedRectangleBorder(
-//                               borderRadius: BorderRadius.circular(20),
-//                             ),
-//                             textStyle: TextStyle(color: Colors.white),
-//                           ),
-//                           child: Ink(
-//                             decoration: BoxDecoration(
-//                               gradient: LinearGradient(
-//                                 colors: [
-//                                   AppColors.gradientStart,
-//                                   AppColors.gradientMiddle,
-//                                 ],
-//                                 begin: Alignment.topLeft,
-//                                 end: Alignment.topRight,
-//                               ),
-//                               borderRadius: BorderRadius.circular(30),
-//                             ),
-//                             child: Container(
-//                               constraints: BoxConstraints(
-//                                   minWidth: 88.0, minHeight: 36.0),
-//                               alignment: Alignment.center,
-//                               child: Text(
-//                                 AppStrings.login,
-//                                 style: TextStyle(
-//                                     fontSize: 18, color: Colors.white),
-//                               ),
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> sendOTP(String type, String contact) async {
-//     String url = AppStrings.sendOtpUrl;
-
-//     Map<String, String> body = {"type": type, "contact": contact};
-//     print('Sending OTP with type: $type and contact: $contact');
-
-//     try {
-//       final response = await http.post(
-//         Uri.parse(url),
-//         headers: {"Content-Type": "application/json"},
-//         body: jsonEncode(body),
-//       );
-
-//       if (response.statusCode == 200) {
-//         Map<String, dynamic> data = jsonDecode(response.body);
-//         print('Response: ${data["message"]}');
-//         Get.to(OtpScreen(), arguments: {'type': type, 'contact': contact});
-//       } else {
-//         print('Failed to send OTP. Error: ${response.body}');
-//         Get.snackbar(
-//           AppStrings.warning,
-//           'Failed to send OTP. Please try again.',
-//           backgroundColor: Colors.red,
-//           colorText: Colors.white,
-//         );
-//       }
-//     } catch (error) {
-//       print('Failed to connect to the server. Error: $error');
-//       Get.snackbar(
-//         AppStrings.warning,
-//         'Failed to connect to the server. Please check your internet connection and try again.',
-//         backgroundColor: Colors.red,
-//         colorText: Colors.white,
-//       );
-//     }
-//   }
-
-//   void _saveText(String text) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     prefs.setString('storedText', text);
-//     storedText.value = text;
-//   }
-// }
-
-// class AppBarContent extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 320,
-//       decoration: BoxDecoration(
-//         image: DecorationImage(
-//           image: AssetImage(AppStrings.appBarLoginImage),
-//           fit: BoxFit.fill,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
