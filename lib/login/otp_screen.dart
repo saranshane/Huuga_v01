0e271a16bbabb189c320e22e73a3314c807fe95f
saranshane <saranshane01@gmail.com>
2024-05-23 13:28:43 +0530

    OTP UPDATE

--- lib/login/otp_screen.dart
+++ lib/login/otp_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
-import 'package:login/constants.dart'; // Importing the constants file
+import 'package:login/constants.dart';
 import 'package:login/Registration/registration_screen.dart';
 
 class OtpScreen extends StatelessWidget {
@@ -51,82 +51,94 @@ class OtpScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: Text(
-                    AppStrings.enterOtpSentTo,
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 5),
-                  child: Text(
-                    contact,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: List.generate(
-                      AppConstants.otpDigits,
-                      (index) => SizedBox(
-                        width: 50,
-                        child: TextField(
-                          controller: otpController.controllers[index],
-                          focusNode: otpController.focusNodes[index],
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          maxLength: 1,
-                          onChanged: (String value) {
-                            if (value.length == 1 &&
-                                index < AppConstants.otpDigits - 1) {
-                              otpController.focusNodes[index].unfocus();
-                              FocusScope.of(context).requestFocus(
-                                  otpController.focusNodes[index + 1]);
-                            } else if (value.isEmpty && index > 0) {
-                              otpController.focusNodes[index].unfocus();
-                              FocusScope.of(context).requestFocus(
-                                  otpController.focusNodes[index - 1]);
-                            }
-                          },
-                          decoration: InputDecoration(
-                            counterText: "",
-                            border: OutlineInputBorder(),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: Text(
+                            AppStrings.enterOtpSentTo,
+                            style: TextStyle(fontSize: 18),
                           ),
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: GestureDetector(
-                    onTap: () {
-                      otpController.resendOTP();
-                    },
-                    child: Obx(() => Text(
-                          '${AppStrings.resendOtpIn}${otpController.timerSeconds.value} seconds',
-                          style: TextStyle(fontSize: 16, color: Colors.blue),
-                        )),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: verifyButtonDecoration,
-                  child: TextButton(
-                    onPressed: () {
-                      String otp = otpController.controllers.fold(
-                          "",
-                          (previousValue, controller) =>
-                              previousValue + controller.text);
-                      otpController.verifyOTP(otp, type, contact);
-                    },
-                    child: Text(
-                      AppStrings.verifyButtonText,
-                      style: TextStyle(color: Colors.white),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5),
+                          child: Text(
+                            contact,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: List.generate(
+                              AppConstants.otpDigits,
+                              (index) => SizedBox(
+                                width: 50,
+                                child: TextField(
+                                  controller: otpController.controllers[index],
+                                  focusNode: otpController.focusNodes[index],
+                                  keyboardType: TextInputType.number,
+                                  textAlign: TextAlign.center,
+                                  maxLength: 1,
+                                  onChanged: (String value) {
+                                    if (value.length == 1 &&
+                                        index < AppConstants.otpDigits - 1) {
+                                      otpController.focusNodes[index].unfocus();
+                                      FocusScope.of(context).requestFocus(
+                                          otpController.focusNodes[index + 1]);
+                                    } else if (value.isEmpty && index > 0) {
+                                      otpController.focusNodes[index].unfocus();
+                                      FocusScope.of(context).requestFocus(
+                                          otpController.focusNodes[index - 1]);
+                                    }
+                                  },
+                                  decoration: InputDecoration(
+                                    counterText: "",
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: GestureDetector(
+                            onTap: () {
+                              otpController.resendOTP();
+                            },
+                            child: Obx(() => Text(
+                                  '${AppStrings.resendOtpIn}${otpController.timerSeconds.value} seconds',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.blue),
+                                )),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: verifyButtonDecoration,
+                          child: TextButton(
+                            onPressed: () {
+                              String otp = otpController.controllers.fold(
+                                  "",
+                                  (previousValue, controller) =>
+                                      previousValue + controller.text);
+                              otpController.verifyOTP(otp, type, contact);
+                            },
+                            child: Text(
+                              AppStrings.verifyButtonText,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
